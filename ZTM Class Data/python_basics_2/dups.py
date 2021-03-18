7284134b7788d3a9544227c54ea4ecbe5a3dcdd4
Chris <cjmugs@gmail.com>
2021-03-18 15:21:39 -0500

    new files added

--- ZTM Class Data/python_basics_2/dups.py
+++ ZTM Class Data/python_basics_2/dups.py
@@ -1,2 +1,12 @@
 # Finding dups in a list
-list = ['a', 'b', 'c', 'd', 'e', 'e', 'm', 'm']
+list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# empty list that will get populated with and dups
+duplicates = []
+
+# Loop over the list 
+for value in list:
+    if list.count(value) > 1:
+        if value not in duplicates:
+            duplicates.append(value)
+print(duplicates)
