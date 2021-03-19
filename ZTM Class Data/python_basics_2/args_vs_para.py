5620ba3da16c979f06b8b5da3eaaf23142010577
Chris <cjmugs@gmail.com>
2021-03-18 21:51:54 -0500

    updated file

--- ZTM Class Data/python_basics_2/args_vs_para.py
+++ ZTM Class Data/python_basics_2/args_vs_para.py
@@ -6,3 +6,7 @@ def say_hello(name, emoji):
 # arguements = what is inside of the brackets
 # arguements call the function
 say_hello('Chris', ':)')
+
+# keyword arguements
+# giving the arguements keywords and values
+say_hello(emoji=':)', name='Thomas')
