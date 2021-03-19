848c10d42d8e7054b88d6924715def79963f29f1
Chris <cjmugs@gmail.com>
2021-03-18 21:45:46 -0500

    updated file

--- ZTM Class Data/python_basics_2/args_vs_para.py
+++ ZTM Class Data/python_basics_2/args_vs_para.py
@@ -1,8 +1,8 @@
 # a basic function with parameter inside the brackets
 # parameters define the function
-def say_hello(name):
-    print(f'whats up {name}!')
+def say_hello(name, emoji):
+    print(f'whats up {name} {emoji} !')
 
 # arguements = what is inside of the brackets
 # arguements call the function
-say_hello('Chris')
+say_hello('Chris', ':)')
