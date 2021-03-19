8064d6e93f6f7dc436bee0445e025dc4e9b8c28f
Chris <cjmugs@gmail.com>
2021-03-19 15:41:25 -0500

    updated files

---
+++ ZTM Class Data/python_basics_2/args_vs_kwargs.py
@@ -0,0 +1,10 @@
+# *args  **kwargs
+
+# Example of *args
+def super_function(*args):
+    print(*args)
+    return sum(args)
+super_function(1,2,3,4,5)
+print(super_function(1,2,3,4,5))
+
+# Example of *kwargs
