61ced0a0bb2851fdabbb3de6b4e8fd7126cbdb09
Chris <cjmugs@gmail.com>
2021-03-19 11:45:17 -0500

    new file

---
+++ ZTM Class Data/python_basics_2/methods_vs_functions.py
@@ -0,0 +1,9 @@
+# Methods vs Functions
+# Example of a function --> list()
+# Example of a method --> .capitalize()
+
+
+# A basic function with a method in it (also has a default args)
+def name(name='melanie'):
+    print(name.capitalize())
+name('max')
