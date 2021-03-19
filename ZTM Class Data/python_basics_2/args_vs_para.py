4b23f0c36afad8690263d88494aa0ae9c4fcf612
Chris <cjmugs@gmail.com>
2021-03-18 21:41:58 -0500

    updated file

--- ZTM Class Data/python_basics_2/args_vs_para.py
+++ ZTM Class Data/python_basics_2/args_vs_para.py
@@ -1,6 +1,8 @@
 # a basic function with parameter inside the brackets
+# parameters define the function
 def say_hello(name):
     print(f'whats up {name}!')
 
 # arguements = what is inside of the brackets
+# arguements call the function
 say_hello('Chris')
