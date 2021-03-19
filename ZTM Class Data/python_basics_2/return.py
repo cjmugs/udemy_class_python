b0e0b3478d7ad6e8fdd617f9cf8ac8a1ed3ba639
Chris <cjmugs@gmail.com>
2021-03-19 11:13:55 -0500

    updated files

--- ZTM Class Data/python_basics_2/return.py
+++ ZTM Class Data/python_basics_2/return.py
@@ -8,3 +8,12 @@ print(sum(10,5))
 # a function should do one thing really well
 # a function should RETURN something
 
+def adding(num1, num2):
+    def another_func(num1, num2):
+        return num1 + num2
+    return another_func(num1, num2)
+
+total = adding(10, 20)
+print(total)
+
+# Return automatically exits the function
