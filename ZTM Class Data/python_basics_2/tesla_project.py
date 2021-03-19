04f0ac82e6283126f4ad30bd3b317e5c9b514884
Chris <cjmugs@gmail.com>
2021-03-19 11:26:08 -0500

    updated file

--- ZTM Class Data/python_basics_2/tesla_project.py
+++ ZTM Class Data/python_basics_2/tesla_project.py
@@ -1,6 +1,3 @@
-
-
-
 def checkDriverAge(age=0 ):
     if int(age) < 18:
 	    print("Sorry, you are too young to drive this car. Powering off")
@@ -9,6 +6,8 @@ def checkDriverAge(age=0 ):
     elif int(age) == 18:
 	    print("Congratulations on your first year of driving. Enjoy the ride!")
 checkDriverAge(92)
+
+
 #1. Wrap the above code in a function called checkDriverAge(). Whenever you call this function, you will get prompted for age. 
 # Notice the benefit in having checkDriverAge() instead of copying and pasting the function everytime?
 
