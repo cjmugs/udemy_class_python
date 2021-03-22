6e3b2ae7317b408dbd7644b37cecc9697e74aae8
cjmugs <cjmugs@gmail.com>
2021-03-22 14:21:38 -0500

    updated file

--- ZTM Class Data/python_basics_2/walrus_operator.py
+++ ZTM Class Data/python_basics_2/walrus_operator.py
@@ -4,6 +4,12 @@
 # Example
 string = "Hello, worlds!"
 
-if (len(string) > 10):
+if ((n:= len(string)) > 10):
     print(f"too long {len(string)} elements")
 
+# Another example 
+# loops through and prints until it cant anymore
+while (n := len(string) > 1):
+    print(string)
+    string = string[:-1]
+print(string)
