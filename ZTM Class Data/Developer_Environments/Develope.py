2f8c3b709b77889bdf27ddadd9e316676b2e978a
cjmugs <cjmugs@gmail.com>
2021-03-23 15:51:10 -0500

    updated file

--- ZTM Class Data/Developer_Environments/Develope.py
+++ ZTM Class Data/Developer_Environments/Develope.py
@@ -3,5 +3,16 @@ a = 'TEST'
 print(a)
 
 
-def develope_tool():
+def driver():
     pass
+
+
+def checkDriverAge(age=0 ):
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+        
+checkDriverAge(92)
