cd8f88478b0fb51c5a292308629f03d8f448ffa6
cjmugs <cjmugs@gmail.com>
2021-03-26 14:44:43 -0500

    updates and new file

--- ZTM Class Data/Advance_Python/oop_Exercise_cats.py
+++ ZTM Class Data/Advance_Python/oop_Exercise_cats.py
@@ -1,4 +1,7 @@
 #Given the below class:
+from typing import cast
+
+
 class Cat:
     species = 'mammal'
     def __init__(self, name, age):
@@ -7,13 +10,14 @@ class Cat:
 
 
 # 1 Instantiate the Cat object with 3 cats
-cat1 = Cat('Fluffy', 10)
-cat2 = Cat('Mittens', 6)
-cat3 = Cat('Dr. Xavier Jones Burmingham the third', 102) 
+Fluffy = Cat('Fluffy', 10)
+Mittens = Cat('Mittens', 6)
+Doctor_Xavier_Jones_Burmingham_the_third = Cat('Dr. Xavier Jones Burmingham the third', 102) 
 
 
 # 2 Create a function that finds the oldest cat
-
-
-
+def oldest_cat(*args):
+    return max(args)
+    
 # 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
+print(f'The oldest cat is {oldest_cat(Fluffy.age, Mittens.age, Doctor_Xavier_Jones_Burmingham_the_third.age)} years old')
