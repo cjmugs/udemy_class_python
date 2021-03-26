a52d9aa705a03d4469ec371b0a9df45578841e98
cjmugs <cjmugs@gmail.com>
2021-03-26 14:07:48 -0500

    updates and new file

--- ZTM Class Data/Advance_Python/Attributes_Methods.py
+++ ZTM Class Data/Advance_Python/Attributes_Methods.py
@@ -2,23 +2,22 @@
 class PlayerCharacter:
     # Class object attribute
     membership = True
+
     def __init__(self, name, age):
         self.name = name
         self.age =age
 
-    def run(self):
-        print('Run')
-        return 'done'
-
+# A method to
+    def shout(self):
+        print(f'Hello my name is {self.name} and my age is {self.age}')
+     
+# Instances of the class PlayerCharacter
 player1 = PlayerCharacter('Max', 2)
 player2 = PlayerCharacter('Melanie', 8)
 player3 = PlayerCharacter('Chris', 39)
 player4 = PlayerCharacter('Kim', 38)
 
-print(player1.run())
-
-print(player1.name, player1.age)
-print(player2.name, player2.age)
-print(player3.name, player3.age)
-print(player4.name, player4.age)
-print(player1.membership, player2.membership, player3.membership, player4.membership) 
+print(player1.shout())
+print(player2.shout())
+print(player3.shout())
+print(player4.shout())
