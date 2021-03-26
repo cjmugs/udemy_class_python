bed39541a86af76a4294a4f34422d3e9872043cf
cjmugs <cjmugs@gmail.com>
2021-03-26 12:51:38 -0500

    updates and new file

---
+++ ZTM Class Data/Advance_Python/Attributes_Methods.py
@@ -0,0 +1,23 @@
+
+class PlayerCharacter:
+    membership = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age =age
+
+    def run(self):
+        print('Run')
+        return 'done'
+
+player1 = PlayerCharacter('Max', 2)
+player2 = PlayerCharacter('Melanie', 8)
+player3 = PlayerCharacter('Chris', 39)
+player4 = PlayerCharacter('Kim', 38)
+
+print(player1.run())
+
+print(player1.name, player1.age)
+print(player2.name, player2.age)
+print(player3.name, player3.age)
+print(player4.name, player4.age)
+print(player1.membership, player2.membership, player3.membership, player4.membership) 
