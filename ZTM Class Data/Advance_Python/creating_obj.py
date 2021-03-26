f0fe9c456a89786120d55dcb53aaeed648a603bb
cjmugs <cjmugs@gmail.com>
2021-03-26 12:39:40 -0500

    updates and new file

--- ZTM Class Data/Advance_Python/creating_obj.py
+++ ZTM Class Data/Advance_Python/creating_obj.py
@@ -1,13 +1,22 @@
 # Creating our own oop
 
 class PlayerCharacter:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age =age
 
     def run(self):
         print('Run')
-player1 = PlayerCharacter('Max')
-player2 = PlayerCharacter('Melanie')
+        return 'done'
 
-print(player1.name)
-print(player2.name)
+player1 = PlayerCharacter('Max', 2)
+player2 = PlayerCharacter('Melanie', 8)
+player3 = PlayerCharacter('Chris', 39)
+player4 = PlayerCharacter('Kim', 38)
+
+print(player1.run())
+
+print(player1.name, player1.age)
+print(player2.name, player2.age)
+print(player3.name, player3.age)
+print(player4.name, player4.age)
