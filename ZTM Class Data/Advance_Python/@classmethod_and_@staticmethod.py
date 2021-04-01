8a8edc150292602bfcaff1e7a2bb685ed130c571
Chris <cjmugs@gmail.com>
2021-04-01 16:03:14 -0500

    updates

--- ZTM Class Data/Advance_Python/@classmethod_and_@staticmethod.py
+++ ZTM Class Data/Advance_Python/@classmethod_and_@staticmethod.py
@@ -4,10 +4,13 @@ class PlayerCharacter:
         self.name = name
         self.age = age
        
-       
     def shout(self):
         print(f'Hello my name is {self.name} and my age is {self.age}')
 
+    @classmethod
+    def adding_things(cls, num1, num2):
+        return num1 + num2
+
 player1 = PlayerCharacter('Max', 20)
 
-print(player1.shout())
+print(player1.adding_things(2, 7))
