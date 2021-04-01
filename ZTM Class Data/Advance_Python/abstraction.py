46b7d3a3acc300e8d4fadbc3cfdea8eb2d94d4db
Chris <cjmugs@gmail.com>
2021-04-01 16:45:42 -0500

    updates

--- ZTM Class Data/Advance_Python/abstraction.py
+++ ZTM Class Data/Advance_Python/abstraction.py
@@ -7,12 +7,14 @@ class Crew_Member:
     def member(self):
         print(f'Crew Member name {self.name} and years paddling {self.paddleyear}')
 
+#Instanced the class 
 crew1 = Crew_Member('Chris' , 10)
 crew2 = Crew_Member('Steve' , 7)
 crew3 = Crew_Member('Scott' , 7)
 crew4 = Crew_Member('Ben' , 5)
 crew5 = Crew_Member('Robbie' , 4)
 
+# Call the instance and method
 crew1.member()
 crew2.member()
 crew3.member()
