33d98692ff3e755b2e1f37b42190d5355d47d64e
Owner <cjmugs@gmail.com>
2021-04-08 08:28:23 -0500

    updates

--- ZTM Class Data/Advance_Python/inheritance.py
+++ ZTM Class Data/Advance_Python/inheritance.py
@@ -2,7 +2,13 @@
 
 class User():
     def sign_in(self):
-        print('Logged in')
+        # Global variable
+        power = self.power
+        
+        if (power > 10):
+            print('Logged in')
+        else:
+            print('Sorry not powerful enough to enter')
         
         # inherited from the parent class inside the () // Member is the subclass of User
 class Member(User):
@@ -12,9 +18,11 @@ class Member(User):
 
     def attack(self):
         print(f'attacking with {self.power}')
-        
+
 #Instance of the parent class
 user1 = Member('Bill', 50)
 
+# Calling the class/ instance of that class and method
 user1.sign_in()
 user1.attack()
+
