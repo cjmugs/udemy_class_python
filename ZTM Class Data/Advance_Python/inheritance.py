fb0137e30c41f688137321483221661a125b8a20
Owner <cjmugs@gmail.com>
2021-04-08 08:10:09 -0500

    updates

--- ZTM Class Data/Advance_Python/inheritance.py
+++ ZTM Class Data/Advance_Python/inheritance.py
@@ -1,6 +1,12 @@
 # inheritance example
+
 class User():
     def sign_in(self):
         print('Logged in')
-class Member():
+        
+        # inherited from the parent class inside the ()
+class Member(User):
     pass
+
+user1 = Member()
+print(user1.sign_in())
