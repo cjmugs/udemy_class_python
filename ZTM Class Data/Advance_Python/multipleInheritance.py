8e5b99d479e37116087237cd893d20831de5b2e4
Owner <cjmugs@gmail.com>
2021-04-13 19:39:58 -0500

    updates

--- ZTM Class Data/Advance_Python/multipleInheritance.py
+++ ZTM Class Data/Advance_Python/multipleInheritance.py
@@ -1,5 +1,29 @@
-# inheritance example
-
 class User():
     def sign_in(self):
-      
+        print('Please sign in . . . ')
+
+        
+        # inherited from the parent class inside the () // Member is the subclass of User
+class Human(User):
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f'attacking with {self.power}')
+
+class Monster(Human):
+    def __init__(self, name, power, sp_abilty):
+        self.sp_abilty = sp_abilty
+        Human.__init__(self, name, power)
+       
+
+# Instance of the parent class
+person1 = Human('Bill', 50)
+monster1 = Monster('Goldbar', 150, "fire")
+
+# Calling the class/ instance of that class and method
+person1.sign_in()
+person1.attack()
+monster1.attack()
+print(person1.name)
