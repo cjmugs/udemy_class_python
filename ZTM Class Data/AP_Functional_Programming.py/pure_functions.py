06328aa44ac993de915c4e06b68e4775699fff5b
Owner <cjmugs@gmail.com>
2021-04-19 08:23:26 -0500

    updates

--- ZTM Class Data/AP_Functional_Programming.py/pure_functions.py
+++ ZTM Class Data/AP_Functional_Programming.py/pure_functions.py
@@ -3,4 +3,14 @@ def multiply_by_2(li):
     for item in li:
         new_list.append(item*2)
     return new_list
+
+
+
+def divide_by_2(li):
+    new_list2 = []
+    for item in li:
+        new_list2.append(item/2)
+    return new_list2
+
 print(multiply_by_2([1,2,3]))
+print(divide_by_2([20,18,16]))
