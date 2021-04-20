9381a8e40858e62277dae77bb157021fb6315e8f
Owner <cjmugs@gmail.com>
2021-04-20 17:17:27 -0500

    updates

--- ZTM Class Data/AP_Functional_Programming.py/lambda_expression.py
+++ ZTM Class Data/AP_Functional_Programming.py/lambda_expression.py
@@ -2,3 +2,9 @@ my_list = [1,2,3]
 print(my_list)
 # lambda is a ONE time function
 print(list(map(lambda item: item*2, my_list)))
+
+
+# Another example
+
+list_2 = ["Hello", "Good", "Bye"]
+print(list(map(lambda item: item, list_2)))
