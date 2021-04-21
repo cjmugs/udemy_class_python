a9227c1b18eb6efc9cb8ef4c55036ffc6c9f3607
Owner <cjmugs@gmail.com>
2021-04-21 16:07:02 -0500

    updates

--- ZTM Class Data/AP_Functional_Programming.py/list_comprehension.py
+++ ZTM Class Data/AP_Functional_Programming.py/list_comprehension.py
@@ -13,7 +13,12 @@ my_list2 = [num for num in range(0,300)]
 my_list3 = [num*2 for num in range(0,300)]
 
 
+# Another Example with an if condition
+my_list4 = [num**2 for num in range(0,300) if num % 2 ==0]
+
+
 
 print(my_list)
 print(my_list2)
 print(my_list3)
+print(my_list4)
