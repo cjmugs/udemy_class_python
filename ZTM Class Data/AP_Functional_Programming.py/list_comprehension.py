af8deeb01c41c7f5facdd8b8e67b0031283c3cea
Owner <cjmugs@gmail.com>
2021-04-21 16:01:22 -0500

    updates

---
+++ ZTM Class Data/AP_Functional_Programming.py/list_comprehension.py
@@ -0,0 +1,19 @@
+# List Comprehensions
+
+# This does the same as the code below  - - Treat it like a for loop 
+my_list = [char for char in "Jello"]
+
+for char in "Jello":
+    my_list.append(char)
+
+# Another Example
+my_list2 = [num for num in range(0,300)]
+
+# Another Example multiple range * 2
+my_list3 = [num*2 for num in range(0,300)]
+
+
+
+print(my_list)
+print(my_list2)
+print(my_list3)
