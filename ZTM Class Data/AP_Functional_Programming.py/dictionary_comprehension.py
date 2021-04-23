f7db4374a5cbebf9c1083752b8f51df1070ff9bd
Owner <cjmugs@gmail.com>
2021-04-23 09:18:54 -0500

    updates

--- ZTM Class Data/AP_Functional_Programming.py/dictionary_comprehension.py
+++ ZTM Class Data/AP_Functional_Programming.py/dictionary_comprehension.py
@@ -4,10 +4,8 @@ simple_dict = {
     'a': 1,
     'b': 2
 }
-
 # can add for loops and if statements
 my_dict = {key: value**2 for key, value in simple_dict.items() 
-
 # This produces only even using  --> %2==0
 if value%2 == 0}
 print(my_dict)
