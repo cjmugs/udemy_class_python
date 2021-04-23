d4ce0e8a6a62ea0e5c40b47174b4596754c634a2
Owner <cjmugs@gmail.com>
2021-04-23 09:24:05 -0500

    updates

--- ZTM Class Data/AP_Functional_Programming.py/dictionary_comprehension.py
+++ ZTM Class Data/AP_Functional_Programming.py/dictionary_comprehension.py
@@ -9,3 +9,10 @@ my_dict = {key: value**2 for key, value in simple_dict.items()
 # This produces only even using  --> %2==0
 if value%2 == 0}
 print(my_dict)
+
+
+# Another Example
+           # key:value then the for loop
+my_dict2 = {num:num*2 for num in [1,2,3]
+if num%2 == 0}
+print(my_dict2)
