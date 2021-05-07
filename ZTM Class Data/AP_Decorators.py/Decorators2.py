5ad6fbe8270bbea9f531046ecc246eb84d8168ed
Owner <cjmugs@gmail.com>
2021-05-07 08:21:26 -0500

    new files

---
+++ ZTM Class Data/AP_Decorators.py/Decorators2.py
@@ -0,0 +1,17 @@
+# The Power of Decorators (Decorators 2)
+def my_decorator(func):
+    def wrap_func():
+        print('***************')
+        func()
+        print('***************')
+    return wrap_func()
+
+@my_decorator
+def hello_hi():
+    print('Hellollloooo')
+
+@my_decorator
+def bye():
+    print('See ya later')
+hello_hi()
+bye()
--- ZTM Class Data/AP_Decorators.py/decorator.py
+++ ZTM Class Data/AP_Decorators.py/decorator.py
@@ -7,22 +7,5 @@ def greet():
 a = hello(greet)
 print(a)
 
-# The Power of Decorators (Decorators 2)
-def my_decorator(func):
-    def wrap_func():
-        print('***************')
-        func()
-        print('***************')
-    return wrap_func()
 
-@my_decorator
-def hello_hi():
-    print('Hellollloooo')
 
-@my_decorator
-def bye():
-    print('See ya later')
-hello_hi()
-bye()
-
-# (Decorators 3)
