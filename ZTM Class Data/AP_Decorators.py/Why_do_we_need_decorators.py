fc2b9dec3f8f4d0792031b618d2161078c13cbed
Owner <cjmugs@gmail.com>
2021-05-28 08:32:22 -0500

    updates

--- ZTM Class Data/AP_Decorators.py/Why_do_we_need_decorators.py
+++ ZTM Class Data/AP_Decorators.py/Why_do_we_need_decorators.py
@@ -6,13 +6,13 @@ def performance(fn):
     t1 = time()
     result = fn(*args, **kwargs)
     t2 = time()
-    print(f'took {t2-t1}')
+    print(f' It took {t2-t1} s')
     return result
   return wrapper
 
 @performance
 def long_time():
-    for i in range(10000):
+    for i in range(1000000000):
         i*5
 
 long_time()
