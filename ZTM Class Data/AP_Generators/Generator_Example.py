2c60ad7e45bc9991361da9d18eed605c7269a45e
Chris <cjmugs@gmail.com>
2021-06-05 10:27:23 -0500

    updates

---
+++ ZTM Class Data/AP_Generators/Generator_Example.py
@@ -0,0 +1,10 @@
+# Iterable
+
+def make_list(num):
+    result = []
+    for i in range(num):
+        result.append(i*2)
+    return result
+
+my_list =  make_list(100)
+print(list(range(1000000)))
