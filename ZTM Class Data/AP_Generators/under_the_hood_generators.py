858b345f341e265afd19c4e9bf266d771e8117c6
Chris <cjmugs@gmail.com>
2021-06-28 12:15:04 -0500

    updates

--- ZTM Class Data/AP_Generators/under_the_hood_generators.py
+++ ZTM Class Data/AP_Generators/under_the_hood_generators.py
@@ -5,24 +5,26 @@ while True:
         iterator*5
         next(iterator)
     except StopIteration:
-      break
+        break
+      
 class MyGen:
-  current = 0
-  def __init__(self, first, last):
+    current = 0
+def __init__(self, first, last):
     self.first = first
     self.last = last
-    MyGen.current = self.first #this line allows us to use the current number as the starting point for the iteration
+    MyGen.current = self.first
+# this line allows us to use the current number as starting point for iteration
 
-  def __iter__(self):
+def __iter__(self):
     return self
 
-  def __next__(self):
+def __next__(self):
     if MyGen.current < self.last:
-      num = MyGen.current
-      MyGen.current += 1
-      return num
+        num = MyGen.current
+        MyGen.current += 1
+    return num
     raise StopIteration
 
-gen = MyGen(1,100)
+gen = MyGen(1100)
 for i in gen:
     print(i)
