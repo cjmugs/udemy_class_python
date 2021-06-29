1a37a33649f8243add6c3b206fcc30eabc8e6657
Chris <cjmugs@gmail.com>
2021-06-29 13:50:12 -0500

    updates

---
+++ ZTM Class Data/Modules_in_Python/built_in_modules.py
@@ -0,0 +1,9 @@
+import random
+
+# how to generate a random choice
+print(random.choice([1,2,3,4,5,6,7,8,9]))
+
+# how to shuffle a list
+my_list = [1,2,3,4,5,6,7,8,9]
+random.shuffle(my_list)
+print(my_list)
