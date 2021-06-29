9d711e72608955d9324dfdc7eef2b97e7ca4403c
Chris <cjmugs@gmail.com>
2021-06-29 14:33:02 -0500

    updates

--- ZTM Class Data/Modules_in_Python/guessing_game.py
+++ ZTM Class Data/Modules_in_Python/guessing_game.py
@@ -3,3 +3,17 @@ import sys
 
 # This generates random intergers 
 answer = randint(int(sys.argv[1]), int(sys.argv[2]))
+
+while True:
+    try:
+        guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
+        if 0 < guess < 11:
+            if guess == answer:
+                print('You got it right')
+                break
+            else:
+                print('That is wrong!')
+
+    except ValueError:
+        print('Please enter a number')
+        continue
