2abd1f1cb2af2df668892f06dbb09c1870141127
Chris <cjmugs@gmail.com>
2021-06-29 12:02:18 -0500

    updates

--- ZTM Class Data/Modules_in_Python/different_ways_to_import.py
+++ ZTM Class Data/Modules_in_Python/different_ways_to_import.py
@@ -1,3 +1,4 @@
 # import utility = imports a .py file
 # import shopping.shopping_cart = imports a package or folder
 # from shopping.shopping_cart import buy = imports functions
+# from utility import *  = imports all functions
