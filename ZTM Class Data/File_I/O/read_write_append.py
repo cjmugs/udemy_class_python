02824d4c7e493de7be112c33815964d9d0defe04
Owner <cjmugs@gmail.com>
2021-07-05 20:04:45 -0500

    updates

--- ZTM Class Data/File_I/O/read_write_append.py
+++ ZTM Class Data/File_I/O/read_write_append.py
@@ -1,4 +1,10 @@
+
 # open = opens files
-with open ('Jokes.py') as my_file:
-    print(my_file.readlines())
+# mode w = writes to a file / creates a new one
+with open ('C:/Users/Owner/Desktop/Test.py', mode='w') as my_file:
+    text = my_file.write(' This is where someone like you would write something!')
+
+# mode a = appends to the end of the file
+with open ('C:/Users/Owner/Desktop/Test.py', mode='a') as my_file:
+    text = my_file.write('\n We are adding a lot of data to this file')
 my_file.close()
