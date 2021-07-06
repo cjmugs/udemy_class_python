a6aa6b0565baac3345a18a909368408e3c7bc4f5
Chris <cjmugs@gmail.com>
2021-07-06 09:41:51 -0500

    updates

--- ZTM Class Data/Testing_in_Python/test.py
+++ ZTM Class Data/Testing_in_Python/test.py
@@ -10,7 +10,19 @@ class TestMain(unittest.TestCase):
 	def test_do_stuff2(self):
 		test_param = 'fwdfww'
 		result = main.do_stuff(test_param)
-		self.asserIsInstance(result, ValueError)
+		self.assertIsInstance(result, ValueError)
 
-unittest.main()
+
+	def test_do_stuff3(self):
+		test_param = None
+		result = main.do_stuff(test_param)
+		self.assertEqual(result, 'Please enter a number')
+
+
+	def test_do_stuff4(self):
+		test_param = ''
+		result = main.do_stuff(test_param)
+		self.assertEqual(result, 'Please enter a number')
+if __name__ == '__main__':
+	unittest.main()
 
