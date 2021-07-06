885a93a7bf55319df465e7d6f6f3e89ae279638d
Chris <cjmugs@gmail.com>
2021-07-05 22:37:46 -0500

    updates

--- ZTM Class Data/Regular_Expression/regular_expressions.py
+++ ZTM Class Data/Regular_Expression/regular_expressions.py
@@ -1,1 +1,12 @@
-import regular_expressions
+import re
+pattern = re.compile(r"([a-zA-Z]).([a])")
+string = "Fuck my wife annoys the shit out of me!"
+a = pattern.search(string)
+b = pattern.findall(string)
+c = pattern.fullmatch(string)
+d = pattern.match(string)
+
+print(a)
+print(b)
+print(c)
+print(d)
