b5f5446410b383fbb3446492f5b9d7ce3c6a8851
Chris <cjmugs@gmail.com>
2021-07-06 07:37:16 -0500

    updates

--- ZTM Class Data/Regular_Expression/password_validation.py
+++ ZTM Class Data/Regular_Expression/password_validation.py
@@ -2,10 +2,11 @@
 # contains any letters, numbers and $%#@
 import re
 try:
-    pattern = re.compile(r"(^[a-zA-Z0-9_$%#@a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
-    string = 'T@cos1234#.com'
-    a = pattern.search(string)
-    print(a)
+    pattern = re.compile(r"[a-zA-Z0-9$%@#]{8,}\d")
+    password = 'T@cos1234'
+    a = pattern.search(password)
+    check = pattern.fullmatch(password)
+    print(check)
 
 except:
     pass
