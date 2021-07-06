b431d0793b5064ce00f324bf733d2641345f2ad3
Chris <cjmugs@gmail.com>
2021-07-05 21:54:08 -0500

    updates

--- ZTM Class Data/File_I/O/Translator.py
+++ ZTM Class Data/File_I/O/Translator.py
@@ -1,9 +1,15 @@
-import translate
+from translate import Translator
+
+translator= Translator(to_lang="ja")
+translator2= Translator(to_lang="it")
 
 try:
     with open('C:/Users/cjmug/Desktop/Translator.txt', mode='r') as file:
         read = file.read()
-        print(read)
+        translation = translator.translate(read)
+        translation2 = translator2.translate(read)
+        print(translation)
+        print(translation2)
     file.close()
 
 except FileNotFoundError as err:
