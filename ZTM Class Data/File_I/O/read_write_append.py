b2984790f152ca112e5976a52eb420f2abfdfadb
Owner <cjmugs@gmail.com>
2021-07-05 20:09:23 -0500

    updates

--- ZTM Class Data/File_I/O/read_write_append.py
+++ ZTM Class Data/File_I/O/read_write_append.py
@@ -1,10 +1,14 @@
 
 # open = opens files
 # mode w = writes to a file / creates a new one
-with open ('C:/Users/Owner/Desktop/Test.py', mode='w') as my_file:
-    text = my_file.write(' This is where someone like you would write something!')
+try:
+    with open ('C:/Users/Owner/Desktop/Test.py', mode='w') as my_file:
+        text = my_file.write(' This is where someone like you would write something!')
 
-# mode a = appends to the end of the file
-with open ('C:/Users/Owner/Desktop/Test.py', mode='a') as my_file:
-    text = my_file.write('\n We are adding a lot of data to this file')
-my_file.close()
+    # mode a = appends to the end of the file
+    with open ('C:/Users/Owner/Desktop/Test.py', mode='a') as my_file:
+        text = my_file.write('\n We are adding a lot of data to this file')
+    my_file.close()
+    
+except FileNotFoundError as err:
+    print("Shit Doesn't Exist yo")
