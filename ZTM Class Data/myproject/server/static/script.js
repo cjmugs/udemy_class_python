eeb54a2150d9963c41ae00f12ee7a7cc9cfaca70
unknown <cjmugs@gmail.com>
2021-07-29 14:58:12 -0500

    bn

--- ZTM Class Data/myproject/server/static/script.js
+++ ZTM Class Data/myproject/server/static/script.js
@@ -1,2 +1,7 @@
-console.log('Hello and thanks for accessing me . . .')
+
 alert("THIS IS ONLY A TEST")
+
+function myFunction() {
+    document.getElementById("button").innerHTML = "YOU CLICKED ME!";}
+
+console.log('Hello and thanks for accessing me . . .')
