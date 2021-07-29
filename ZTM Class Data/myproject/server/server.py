47c25416669cd2b0a9fd414a104864218c90d09c
unknown <cjmugs@gmail.com>
2021-07-29 14:26:02 -0500

    bn

--- ZTM Class Data/myproject/server/server.py
+++ ZTM Class Data/myproject/server/server.py
@@ -1,13 +1,17 @@
 # Min for a FLask server to work 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>HELLO CHRIS, THIS IS JUST A TEST SITE <br> FOR DEVELOPMENT MODE ONLY</p>"
+    return render_template('index.html')
+
+@app.route("/blog")
+def blog():
+    return "<p>THIS IS JUST A TEST SITE ABOUT BLOGS <br> FOR DEVELOPMENT MODE ONLY</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
