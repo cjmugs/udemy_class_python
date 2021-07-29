988e6b0f72c848e04925e3923551446f09ed26c2
unknown <cjmugs@gmail.com>
2021-07-29 14:11:51 -0500

    bn

--- ZTM Class Data/myproject/server/server.py
+++ ZTM Class Data/myproject/server/server.py
@@ -3,7 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello Chris!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
