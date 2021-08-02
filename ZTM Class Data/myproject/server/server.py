c40df3ca7b0e43f10b246c6dc610300734aaa785
unknown <cjmugs@gmail.com>
2021-08-02 14:35:11 -0500

    df

--- ZTM Class Data/myproject/server/server.py
+++ ZTM Class Data/myproject/server/server.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
-@app.route("/")
+@app.route("/")                       
 def home_page():
     return render_template('index.html')
 
